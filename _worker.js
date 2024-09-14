baa893589b85274ab50c22b0a91c574ca5a65ca9
ilrazer00 <ilhamrazer00@gmail.com>
2024-09-14 09:17:06 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bbe03934-3b81-486d-8e86-c10bc45c1649';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
